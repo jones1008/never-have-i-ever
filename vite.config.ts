6067f00d9411c27e5a786ba5d5aac6e5ee6c745b
jones1008 <jones@fluhrers.de>
2021-04-16 17:49:53 +0200

    first try adding PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,45 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+    plugins: [
+        vue(),
+        VitePWA({
+            manifest: {
+                name: "Ich hab' noch nie",
+                short_name: "Ich hab' noch nie",
+                display: "standalone",
+                theme_color: "#F67730",
+                background_color: "#F67730",
+                description: "Ein einfaches Partyspiel mit Fragen, bei der man auch selbst Fragen hinzufügen kann",
+                icons: [{
+                    src: "/img/icons/homescreen48.png",
+                    sizes: "48x48",
+                    type: "image/png"
+                }, {
+                    src: "/img/icons/homescreen72.png",
+                    sizes: "72x72",
+                    type: "image/png"
+                }, {
+                    src: "/img/icons/homescreen96.png",
+                    sizes: "96x96",
+                    type: "image/png"
+                }, {
+                    src: "/img/icons/homescreen144.png",
+                    sizes: "144x144",
+                    type: "image/png"
+                }, {
+                    src: "/img/icons/homescreen168.png",
+                    sizes: "168x168",
+                    type: "image/png"
+                }, {
+                    src: "/img/icons/homescreen192.png",
+                    sizes: "192x192",
+                    type: "image/png"
+                }]
+            }
+        })
+    ]
 })
